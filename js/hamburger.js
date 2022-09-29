7e3c03d9d81c97dd3e5a725363c9c7cbf39ea29e
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2022-09-30 00:59:07 +0200

    hamburger works

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,11 +1,25 @@
-const menu = document.querySelector(".menu");
+// const menu = document.querySelector(".menu");
 
-const nav = document.querySelector(".stroke");
+// const nav = document.querySelector("#main-nav");
+
+// menu.addEventListener("click", () => {
+//   if (nav.classList.contains("open-nav")) {
+//     nav.classList.remove("open-nav");
+//   } else {
+//     nav.classList.add("open-nav");
+//   }
+// });
+
+// trying again
+
+const menu = document.querySelector(".menu");
+const close = document.querySelector(".close");
+const nav = document.querySelector("#main-nav");
 
 menu.addEventListener("click", () => {
-  if (nav.classList.contains("open-nav")) {
-    nav.classList.remove("open-nav");
-  } else {
-    nav.classList.add("open-nav");
-  }
+  nav.classList.add("open-nav");
+});
+
+close.addEventListener("click", () => {
+  nav.classList.remove("open-nav");
 });
